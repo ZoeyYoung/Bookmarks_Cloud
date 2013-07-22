68dc9808a048a98d11a52f86496edbf924f1298c
ZoeyYoung <ydingmiao@gmail.com>
2013-07-22 17:35:46 +0800

    删除注释掉的代码

--- bookmarks_cloud/utils.py
+++ bookmarks_cloud/utils.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# import urllib2
-# from bs4 import BeautifulSoup
 from tornado import httpclient
-# import lxml.html
-# from lxml import etree
 from readability.readability import Document
 from functools import lru_cache
+import math
 
 
 # 格式化标签
@@ -23,6 +20,9 @@ def format_tags(str):
 # TODO(Zoey) 需要调整到尽量快, 主要是为了防止连续多次抓取同一个网站
 @lru_cache(maxsize=32)
 def get_html(url):
+    # request = urllib2.Request(url)
+    # request.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1485.0 Safari/537.36')
+    # html_doc = urllib2.urlopen(request, timeout=10)
     http_client = httpclient.HTTPClient()
     request = httpclient.HTTPRequest(url)
     response = http_client.fetch(request)
@@ -33,60 +33,12 @@ def get_html(url):
 def get_bookmark_info(url):
     html = get_html(url)
     print(get_html.cache_info())
-    # import chardet
-    # encoding = chardet.detect(html)['encoding']
-    # htmlEl = etree.HTML(html.decode(encoding, 'ignore'))
-
-    # title = htmlEl.find(".//title").text.encode('utf-8')
     title = Document(html).short_title()
     article = Document(html).summary()
-    # description = htmlEl.xpath("//meta[translate(@name, 'ABCDEFGHJIKLMNOPQRSTUVWXYZ', 'abcdefghjiklmnopqrstuvwxyz')='description']")
-    # if len(description) > 0:
-    #     description = description[0].attrib["content"].encode('utf-8')
-    # else:
-    #     description = ""
     description = Document(html).description()
     keywords = Document(html).keywords()
-    # tags = htmlEl.xpath("//meta[translate(@name, 'ABCDEFGHJIKLMNOPQRSTUVWXYZ', 'abcdefghjiklmnopqrstuvwxyz')='keywords']")
-    # if len(tags) > 0:
-    #     tags = tags[0].attrib["content"].encode('utf-8')
-    # else:
-    #     tags = ""
 
     return dict(title=title, favicon="", article=article, description=description, tags=keywords)
-
-
-# def get_link_info(url):
-#     http_client = httpclient.HTTPClient()
-#     response = http_client.fetch(url)
-#     try:
-#         # request = urllib2.Request(url)
-#         # request.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1485.0 Safari/537.36')
-#         # html_doc = urllib2.urlopen(request, timeout=10)
-#         html_doc = response.body
-#         soup = BeautifulSoup(html_doc)
-#         title = soup.title.string
-#         favicon = soup.select('link[href*="fav"]')
-#         if len(favicon) > 0:
-#             favicon = favicon[0]['href']
-#             if not 'http' in favicon:
-#                 proto, rest = urllib2.splittype(url)
-#                 host, rest = urllib2.splithost(rest)
-#                 favicon = proto + '://' + host + favicon
-#         description = soup.select('meta[name="description"]')
-#         if len(description) > 0:
-#             description = description[0]['content']
-#         else:
-#             description = [text for text in soup.stripped_strings][0]
-#         tags = soup.select('meta[name="keywords"]')
-#         if len(tags) > 0:
-#             tags = tags[0]['content']
-#         http_client.close()
-#         return dict(title=title, favicon=favicon, description=description, tags=tags)
-#     except:
-#         return dict(title='', favicon='', description='', tags='')
-
-import math
 
 
 def get_tags_cloud(tags):
